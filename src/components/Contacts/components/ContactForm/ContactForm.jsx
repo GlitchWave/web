5d7c5bc5a96c94f95ae00783d815190d53f1beff
Ultrasstas <ultrastas51@gmai.com>
2018-07-05 17:34:08 +0300

    [+] form validation

--- src/components/Contacts/components/ContactForm/ContactForm.jsx
+++ src/components/Contacts/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 require('./ContactForm.scss');
 import React from 'react'
+import validator from 'validator';
 
 class ContactForm extends React.PureComponent  {
     constructor(props) {
@@ -16,34 +17,39 @@ class ContactForm extends React.PureComponent  {
             subjectValue: '',
             emailValue: '',
             phoneValue: '',
-            textMessageValue: ''
+            textMessageValue: '',
+            nameClass: '',
+            subjectClass: '',
+            emailClass: '',
+            phoneClass: ''
         }
 
         this.handleInputEvents = this.handleInputEvents.bind(this);
         this.sendMessage = this.sendMessage.bind(this);
+        this.checkAllFields = this.checkAllFields.bind(this);
     }
 
     handleInputEvents(type, payload) {
         switch(type) {
             case 'name':
                 ((typeof payload === 'string') ?
-                  this.setState({nameValue: payload}) :
-                  this.setState({error: Object.assign({}, this.state.error, {name: true})}));
+                  this.setState({nameValue: payload}, this.checkAllFields) :
+                  this.setState({error: Object.assign({}, this.state.error, {name: true})},() => this.checkAllFields()));
                 break;
             case 'subject':
                 ((typeof payload === 'string') ?
-                  this.setState({subjectValue: payload}) :
-                  this.setState({error: Object.assign({}, this.state.error, {subjectValue: true})}));
+                  this.setState({subjectValue: payload}, this.checkAllFields) :
+                  this.setState({error: Object.assign({}, this.state.error, {subjectValue: true})},() => this.checkAllFields()));
                 break;
             case 'email':
                 ((typeof payload === 'string') ?
-                  this.setState({emailValue: payload}) :
-                  this.setState({error: Object.assign({}, this.state.error, {emailValue: true})}));
+                  this.setState({emailValue: payload}, this.checkAllFields) :
+                  this.setState({error: Object.assign({}, this.state.error, {emailValue: true})},() => this.checkAllFields()));
                 break;
             case 'phone':
                 ((typeof payload === 'string') ?
-                  this.setState({phoneValue: payload}) :
-                  this.setState({error: Object.assign({}, this.state.error, {phoneValue: true})}));
+                  this.setState({phoneValue: payload}, this.checkAllFields) :
+                  this.setState({error: Object.assign({}, this.state.error, {phoneValue: true})},() => this.checkAllFields()));
                 break;
             case 'message':
                 this.setState({textMessageValue: payload});
@@ -53,9 +59,23 @@ class ContactForm extends React.PureComponent  {
         }
     }
 
-    sendMessage({ error }) {
-        if(!error.phone || !error.subject || !error.email || !error.phone) {
-        }
+
+    checkAllFields(send) {
+        const { nameValue, subjectValue, emailValue, phoneValue } = this.state,
+              { error } = this.state;
+        
+        ((nameValue.length === 0) ? (error.name = true) : (error.name = false));
+        ((subjectValue.length === 0) ? (error.subject = true) : (error.subject = false));
+        ((!validator.isEmail(emailValue)) ? (error.email = true) : (error.email = false));
+        ((!validator.isMobilePhone(phoneValue, 'any')) ? (error.phone = true) : (error.phone = false));
+
+        this.setState({error: error});
+        if(send) return error;
+    }
+
+    sendMessage() {
+        const { name, phone, email, subject } = this.checkAllFields('send');
+        if(!name && !phone && !email && !subject) console.log('kek');
     }
 
     render() {
@@ -71,21 +91,25 @@ class ContactForm extends React.PureComponent  {
                            placeholder="Name"
                            onInput={(event) => this.handleInputEvents('name', event.target.value)}
                            value={this.state.nameValue}
+                           className={((this.state.error.name) ? 'Error' : '')}
                      />
                     <input type="text"
                            placeholder="Subject"
                            onInput={(event) => this.handleInputEvents('subject', event.target.value)}
                            value={this.state.subjectValue}
+                           className={((this.state.error.subject) ? 'Error' : '')}
                      />
                     <input type="email"
                            placeholder="Email"
                            onInput={(event) => this.handleInputEvents('email', event.target.value)}
                            value={this.state.emailValue}
+                           className={((this.state.error.email) ? 'Error' : '')}
                      />
                     <input type="tel"
                            placeholder="Phone"
                            onInput={(event) => this.handleInputEvents('phone', event.target.value)}
                            value={this.state.phoneValue}
+                           className={((this.state.error.phone) ? 'Error' : '')}
                      />
                     <textarea cols="30"
                               rows="10"
@@ -100,7 +124,8 @@ class ContactForm extends React.PureComponent  {
                         <img src={'https://image.ibb.co/dqhdN8/1_asset_1.png'}/>
                         <span>Attach files</span>
                     </div>
-                    <div className="ContactFormFooterButton" onClick={() => this.sendMessage(this.state)} >SEND</div>
+                    <div className="ContactFormFooterButton"
+                         onClick={this.sendMessage} >SEND</div>
                 </div>
             </div>
         )
