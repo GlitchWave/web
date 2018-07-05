0b0b87e272b15c2a9a24142ab3b12a24d9b94b28
RopeRunner <ultrastas51@gmai.com>
2018-07-06 00:14:21 +0300

    [+] startup skelleton and route

---
+++ src/components/Startup/Startup.jsx
@@ -0,0 +1,12 @@
+require('./Startup.scss');
+import React from 'react';
+
+const Startup = () => {
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Startup;
