e113c39035a562999fa39db0d5b97cf0af355e7d
RopeRunner <glitchwave13@gmail.com>
2018-07-06 03:09:05 +0300

    [+] Startup: Schema left side finished

--- src/components/Startup/Startup.jsx
+++ src/components/Startup/Startup.jsx
@@ -9,18 +9,41 @@ const Startup = () => {
                          backgroundSize: 'cover'
                         }}
              >
-             <h2>Let your dreams come true</h2>
-             <div className="Startup_Banner_Button">Let's do it!</div>
+                <h2>Let your dreams come true</h2>
+                <div className="Startup_Banner_Button">Let's do it!</div>
             </div>
             <div className="Startup_First_Text" >
                 <h2>We offer you</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nostrum! Nobis ad commodi labore autem reiciendis illo perspiciatis accusantium ducimus amet quaerat ullam facere necessitatibus voluptate sapiente dolores sit laborum incidunt neque, voluptatem, quidem atque, eveniet eum? Veritatis, quo numquam!</p>
             </div>
             <h3 className="Startup_Subtitle" >How it works</h3>
-            <div className="Startup_Schema" >
-                <div className="Startup_Schema_Item">
-                    <img src="" alt=""/>
-                    <img src="" alt=""/>
+            <div className="Startup_Schema"
+                 style={{backgroundImage: `url(${'https://image.ibb.co/ioa9Gd/body_rama_img.png'})`,
+                         backgroundSize: 'contain',
+                         backgroundPosition: 'center center'
+                        }} >
+                <div className="Startup_Schema_Item"
+                    >
+                    <img src={'https://image.ibb.co/dQGrwd/body_get_to_know_img.png'}
+                         className="Startup_Schema_Item_Image"
+                        />
+                    <p className="Startup_Schema_Item_Subtitle Right_First">Planning & Preparation</p>
+                    <ul className="Startup_Schema_Item_UL First_UL">
+                        <li>Defining goal</li>
+                        <li>Lorem ipsum dolor sit amet consectetur.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias.</li>
+                    </ul>
+                    <img src={'https://image.ibb.co/fT239y/body_development_img.png'}
+                         className="Startup_Schema_Item_Image"
+                    />
+                    <p className="Startup_Schema_Item_Subtitle Right_Second">Finalizing</p>
+                    <ul className="Startup_Schema_Item_UL Second_UL">
+                        <li>Defining goal</li>
+                        <li>Lorem ipsum dolor sit amet consectetur.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias.</li>
+                        <li>Q/A</li>
+                        <li>Other shit shit shit</li>
+                    </ul>
                 </div>
                 <div className="Startup_Schema_Item">
                     <img src="" alt=""/>
